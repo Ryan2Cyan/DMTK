e94883c7b35e7b2c3699d96b6e9c8e2a0e952ea7
Ryan Bolson (s5326665) <cammellryan@gmail.com>
2024-02-26 19:52:53 +0000

    Created Window Prefab.
    The Window prefab is the bedrock of all UI within DMTK. Each UI based entity (except a few icons) will be contained within a DMTK window.

---
+++ Assets/Scripts/UI/ButtonOnHover.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    /// <summary> Changes the colour of a button's TMP Texts depending on whether the user's cursor is
+    /// hovering over it or not. </summary>
+    public class ButtonOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public List<TextMeshProUGUI> AllTMPTexts;
+        public Color OnHoverColour;
+        public Color OffHoverColour;
+    
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            foreach (var text in AllTMPTexts) text.color = OnHoverColour;
+        }
+        
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            foreach (var text in AllTMPTexts) text.color = OffHoverColour;
+        }
+    }
+}
