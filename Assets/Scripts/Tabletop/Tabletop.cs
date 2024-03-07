ff89d21ba0eed316ab96739f3c2898800ca7e0aa
Ryan Bolson (s5326665) <cammellryan@gmail.com>
2024-03-07 12:02:06 +0000

    Captured and stored centre positions of each grid cell.
    The centre position of each cell can be calculated by taking the second [bottom-right] and fourth [top-left] positions, finding the vector between them, and adding half of that value onto the top-left vector. The positions can be stored in a 2D array of Vector 2s.

--- Assets/Scripts/Tabletop/Tabletop.cs
+++ Assets/Scripts/Tabletop/Tabletop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,14 +12,25 @@ namespace Tabletop
         
         private MeshFilter _meshFilter;
         private MeshRenderer _meshRenderer;
+        private List<List<Vector2>> _gridPositions;
 
         private void Awake()
         {
             _meshFilter = GetComponent<MeshFilter>();
             _meshRenderer = GetComponent<MeshRenderer>();
-            // _meshFilter.mesh = GenerateAsymmetricalGridMesh(TabletopSize, CellSpacing);
-            _meshFilter.mesh = GenerateSymmetricalGridMesh(10);
+            _meshFilter.mesh = GenerateAsymmetricalGridMesh(TabletopSize, CellSpacing, ref _gridPositions);
             _meshRenderer.material = new Material(Shader.Find("Universal Render Pipeline/2D/Sprite-Unlit-Default")) { color = TabletopColour };
+        }
+
+        private void FixedUpdate()
+        {
+            foreach (var gridPositionList in _gridPositions)
+            {
+                foreach (var gridPosition in gridPositionList)
+                {
+                    Debug.DrawLine(new Vector3(gridPosition.x, 0f, gridPosition.y), new Vector3(gridPosition.x, 1f, gridPosition.y), Color.yellow);                    
+                }
+            }
         }
 
         /// <summary>
@@ -28,21 +40,25 @@ namespace Tabletop
         /// <param name="gridSize">Dimensions of the asymmetrical grid, the size.x component corresponding to the
         /// width (x), and size.y corresponding to the depth (z).</param>
         /// <param name="spacing">The distance between each cell, determining the overall surface size of the grid.</param>
+        /// <param name="positions">Stores the central grid position corresponding to each grid cell. </param>
         /// <returns>Generated asymmetrical grid mesh. Can be assigned to the mesh of a MeshFilter component.</returns>
-        private static Mesh GenerateAsymmetricalGridMesh(Vector2Int gridSize, float spacing)
+        private static Mesh GenerateAsymmetricalGridMesh(Vector2Int gridSize, float spacing, ref List<List<Vector2>> positions)
         {
             // Source: https://gist.github.com/mdomrach/a66602ee85ce45f8860c36b2ad31ea14
             
             var mesh = new Mesh();
             var vertices = new List<Vector3>();
             var indices = new List<int>();
-
-            var minimum = new Vector2(spacing * gridSize.x / 2f, spacing * gridSize.y / 2f);
+            positions = new List<List<Vector2>>();
             
+            var minimum = new Vector2(spacing * gridSize.x / 2f, spacing * gridSize.y / 2f);
+
             for (var i = 0; i <= gridSize.x; i++)
             {
+                var jPositions = new List<Vector2>();
                 for (var j = 0; j <= gridSize.y; j++)
                 {
+                    var capturePosition = true;
                     var X = new Vector2(i * spacing - minimum.x, (i + 1) * spacing - minimum.x);
                     var Z = new Vector2(j * spacing - minimum.y, (j + 1) * spacing - minimum.y);
 
@@ -51,11 +67,18 @@ namespace Tabletop
                         vertices.Add(new Vector3(X.x, 0f, Z.x));
                         vertices.Add(new Vector3(X.y, 0, Z.x));
                     }
+                    else capturePosition = false;
 
                     if (j == gridSize.y) continue;
                     vertices.Add(new Vector3(X.x, 0, Z.x));
                     vertices.Add(new Vector3(X.x, 0, Z.y));
+
+                    if (!capturePosition) continue;
+                    var topLeft = new Vector2(X.x, Z.y);
+                    var bottomRight = new Vector2(X.y, Z.x);
+                    jPositions.Add(topLeft + (bottomRight - topLeft) / 2f);
                 }
+                positions.Add(jPositions);
             }
 
             var indicesCount = vertices.Count;
