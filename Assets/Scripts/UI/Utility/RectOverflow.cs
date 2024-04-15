31d19ab92341ee8ac9c2fce5c25aff45cfdd5b5c
Ryan <cammellryan@gmail.com>
2024-04-15 14:14:08 +0100

    (15/04/24) More Features in RectOverflow and Implemented Scroll Rect
    Implemented a few more features in the script that keeps all UI elements within a RectTransform. These features are (1) Changing the horizontal/vertical alignment of elements, (2) Changing the anchors of the elements, (3) Prioritising whether the script should fill out elements into columns or rows. Also changed how the bounds are calculated so that the script's bounds can change with window sizes.
    
    Additionally integrated Unity's ScrollRect with my RectOverflow script.

--- Assets/Scripts/UI/Utility/RectOverflow.cs
+++ Assets/Scripts/UI/Utility/RectOverflow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,32 +10,52 @@ namespace UI.Utility
         [Header("Settings")]
         public Vector2 ElementSize;
         public Vector2 Spacing;
+        public Vector2 Offset;
+        public bool PrioritiseColumns;
+
+        public enum HorizontalAlignment { Left, Right }
+        public enum VerticalAlignment { Up, Down }
+        public HorizontalAlignment RowAlignment = HorizontalAlignment.Right;
+        public VerticalAlignment ColumnAlignment = VerticalAlignment.Down;
         
-        [Header("Components")]
+        public enum Anchor { TopLeft, TopCentre, TopRight, MiddleLeft, MiddleCentre, MiddleRight, BottomLeft, BottomCentre, BottomRight }
+
+        public Anchor ElementAnchor;
+
+        [Header("Components")] 
         public RectTransform RectBounds;
         public List<RectTransform> RectElements;
 
-        private Vector2Int Dimensions;
-        private Vector2 _previousBoundSize;
+        private readonly Vector3[] _boundsCorners = new Vector3[4];
+        private Vector2 _bounds;
+        private Vector2 _elementAnchor;
 
         #region UnityFunctions
 
-        private void Awake()
+        private void OnValidate()
         {
-            RecalculateDimensions();
+                switch (ElementAnchor)
+                {
+                    case Anchor.TopLeft: { _elementAnchor = Vector2.up; } break;
+                    case Anchor.TopCentre: { _elementAnchor = new Vector2(0.5f, 1f); } break;
+                    case Anchor.TopRight: { _elementAnchor = Vector2.one; } break;
+                    case Anchor.MiddleLeft: { _elementAnchor = new Vector2(0f, 0.5f); } break;
+                    case Anchor.MiddleCentre: { _elementAnchor = new Vector2(0.5f, 0.5f); } break;
+                    case Anchor.MiddleRight: { _elementAnchor = new Vector2(1f, 0.5f); } break;
+                    case Anchor.BottomLeft: { _elementAnchor = Vector2.zero; } break;
+                    case Anchor.BottomCentre: { _elementAnchor = new Vector2(0.5f, 0f); } break;
+                    case Anchor.BottomRight: { _elementAnchor = Vector2.right; } break;
+                    default: throw new ArgumentOutOfRangeException();
+                }
         }
 
         private void Update()
         {
-            // // Only execute if the bounds have changed:
-            // if (RectBounds.sizeDelta == _previousBoundSize)
-            // {
-            //     _previousBoundSize = RectBounds.sizeDelta;
-            //     return;
-            // }
-
+            // Calculate bounds:
+            RectBounds.GetLocalCorners(_boundsCorners);
+            _bounds = new Vector2(_boundsCorners[3].x - _boundsCorners[0].x, _boundsCorners[1].y - _boundsCorners[0].y);
+            
             RecalculateDimensions();
-            _previousBoundSize = RectBounds.sizeDelta;
         }
 
         #endregion
@@ -45,41 +66,44 @@ namespace UI.Utility
         {
             var elementTotalSize = ElementSize + Spacing;
             var halfElementTotalSize = elementTotalSize / 2f;
-            var constraints = RectBounds.sizeDelta;
             
-            // // Calculate rows, columns, and elements that fit within the bounds:
-            // var dimensions = new Vector2(Mathf.Floor(constraints.x / elementTotalSize.x), Mathf.Floor(constraints.y / elementTotalSize.y));
+            // Calculate rows, columns, and elements that fit within the bounds:
             var currentIndex = 0;
             var count = Vector2.zero;
             while (currentIndex < RectElements.Count)
             {
+                // Handle element placement:
                 var currentElement = RectElements[currentIndex];
                 currentElement.gameObject.SetActive(true);
-                currentElement.anchoredPosition = new Vector2(count.x, -count.y);
+                currentElement.anchoredPosition = new Vector2(
+                    RowAlignment == HorizontalAlignment.Left ? -(count.x + Offset.x) : count.x + Offset.x,
+                    ColumnAlignment == VerticalAlignment.Down ? -(count.y + Offset.y) : count.y + Offset.y);
                 currentElement.sizeDelta = ElementSize;
-                currentIndex++;
-
-                count.x += halfElementTotalSize.x;
-                if (!(count.x >= constraints.x)) continue;
+                currentElement.anchorMin = _elementAnchor;
+                currentElement.anchorMax = _elementAnchor;
                 
-                count.y += halfElementTotalSize.y;
-                if (count.y >= constraints.y) break;
-                count.x = 0f;
+                // Calculate next row/column:
+                currentIndex++;
+                
+                if (!PrioritiseColumns)
+                {
+                    count.x += halfElementTotalSize.x;
+                    if (!(count.x >= _bounds.x)) continue;
+                    
+                    count.y += halfElementTotalSize.y;
+                    if (count.y >= _bounds.y) break;
+                    count.x = 0f;
+                }
+                else
+                {
+                    count.y += halfElementTotalSize.y;
+                    if (!(count.y >= _bounds.y)) continue;
+
+                    count.x += halfElementTotalSize.x;
+                    if (count.x >= _bounds.x) break;
+                    count.y = 0f;
+                }
             }
-            
-            //
-            // for (var row = 1; row <= dimensions.x; row++)
-            // {
-            //     for (var column = 1; column <= dimensions.y; column++)
-            //     {
-            //         var currentElement = RectElements[currentIndex];
-            //         currentElement.gameObject.SetActive(true);
-            //         currentElement.anchoredPosition = new Vector2(row * halfElementTotalSize.x, column * -halfElementTotalSize.y);
-            //         currentElement.sizeDelta = ElementSize;
-            //         if (currentIndex + 1 >= RectElements.Count) return;
-            //         currentIndex++;
-            //     }
-            // }
             
             // Disable all elements that don't fit within the bounds:
             if (currentIndex >= RectElements.Count) return;
