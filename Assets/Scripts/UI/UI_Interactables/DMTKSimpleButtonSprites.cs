e1375173cb4d57538466c089283a0534278218e6
Ryan <cammellryan@gmail.com>
2024-04-21 22:19:56 +0100

    (21/04/24) Implemented MiniatureLibrary button icon and converted miniature model materials to URP materials.

---
+++ Assets/Scripts/UI/UI_Interactables/DMTKSimpleButtonSprites.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Utility;
+
+namespace UI.UI_Interactables
+{
+    public class DMTKSimpleButtonSprites : MonoBehaviour, UIElement, IPooledObject
+    {
+        [Header("Settings")] 
+        public Sprite UnhighlightedSprite;
+        public Sprite HighlightedSprite;
+        public UnityEvent OnPress;
+
+        [Header("Components")] 
+        public Image ButtonOverlayImage;
+        public bool UIElementActive { get; set; }
+
+        #region UnityFunctions
+
+        protected virtual void Awake()
+        {
+            ButtonOverlayImage.sprite = UnhighlightedSprite;
+        }
+
+        private void OnEnable()
+        {
+            UIElementActive = true;
+        }
+
+        private void OnDisable()
+        {
+            UIElementActive = false;
+        }
+
+        #endregion
+
+        #region InputFunctions
+
+        public void OnMouseUp() { }
+
+        public void OnMouseEnter()
+        {
+            ButtonOverlayImage.sprite = HighlightedSprite;
+        }
+
+        public void OnMouseExit()
+        {
+            ButtonOverlayImage.sprite = UnhighlightedSprite;
+        }
+
+        public void OnDrag()
+        {
+         
+        }
+
+        public virtual void OnMouseDown()
+        {
+            OnPress?.Invoke();
+        }
+
+        #endregion
+
+        public void Instantiate() { }
+        public void Release() { }
+    }
+}
