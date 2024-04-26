a2d1d2ba98e2719e4b78c5019bb67cfd3d8f188d
Ryan <cammellryan@gmail.com>
2024-04-26 19:13:48 +0100

    (26/04/24) Implemented RadialToggleOption custom editor GUI.

---
+++ Assets/Editor/RadialToggleOptionCustomEditor.cs
@@ -0,0 +1,11 @@
+using UI.Miniature_Radial;
+using UnityEditor;
+
+namespace Editor
+{
+    [CustomEditor(typeof(RadialToggleOptions)), CanEditMultipleObjects]
+    public class RadialToggleOptionCustomEditor : RadialToggleCustomCustomEditor
+    {
+        
+    }
+}
